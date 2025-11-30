dcc6b49bf4099163cdd27e652411bf2cf3d773f0
giooo8681-eng <giooo8681@gmail.com>
2025-11-30 09:02:34 +0400

    Create cw01.py

---
+++ day08/classwork/cw01.py
@@ -0,0 +1,5 @@
+#1)კომენტარებში ჩამოწერეთ ყველა control flows ტექნიკა
+# sequence interation selection
+#2)დაწერეთ კოდი for ციკლით, რომლითაც გამოიტანთ თქვენს სახელს 10-ჯერ
+for i in range(10):
+    print("gega")
