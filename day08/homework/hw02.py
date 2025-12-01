439ec0e1182d7244418ecf2c2756ff324336a7ee
giooo8681-eng <giooo8681@gmail.com>
2025-12-01 09:58:30 +0400

    1

---
+++ day08/homework/hw02.py
@@ -0,0 +1,3 @@
+#2)ჩამოწერეთ და ახსენით ყველა Control Flowს ტექნიკა
+# Control Flow ტექნიკები მოიცავს: sequence, selection, iteration 
+
---
+++ day08/homework/hw07.py
@@ -0,0 +1,4 @@
+#7)For ციკლის მეშვეობით ტერმინალში გამოიტანეთ თქვენი საყვარელი საჭმელი 15ჯერ
+favorite_food = "პიცა"
+for i in range(15):
+    print(favorite_food)
