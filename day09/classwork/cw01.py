79dbe7e1109c00c7a0c1ec22a1fd2818dc26e953
giooo8681-eng <giooo8681@gmail.com>
2025-12-07 10:39:04 +0400

    1

--- day09/classwork/cw01.py
+++ day09/classwork/cw01.py
@@ -6,17 +6,21 @@
 
 # 4)გამოიტანეთ რიცხვები 5 დან 50 მდე while loop ის გამოყენებით
 
-while loop რაღაც პირობაზეა დამოკიდებული ხოლო for loop არა
- a=0
- b=30
- for i in range (a, b):
-    print(i)
-  y=0
- while y<30
- print(y)
- y=y+1
- x=5
- while x<50
- print(x)
- x=x+1
+#while loop რაღაც პირობაზეა დამოკიდებული ხოლო for loop არა
+a=0
+b=30
+for i in range(a, b):
+  print(i)  
+
+
+y=0
+while y<30:
+  print(y)
+  y=y+1
+
+
+x=5
+while x<50:
+  print(x)
+  x=x+1
     
