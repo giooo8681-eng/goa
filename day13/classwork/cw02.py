7cecd5de6e804a35699850cc7b0c712f40d5d2c2
giooo8681-eng <giooo8681@gmail.com>
2026-01-11 08:59:09 +0400

    1

---
+++ day13/classwork/cw02.py
@@ -0,0 +1,27 @@
+# 2)შექმენით სია 10 ცალი ელემენტით, 1 დან 10  მდე  რიცხვებით, შემდეგ მომხარებელს შემოატანინეთ რიცხვი და ეგ რიცხვი გამოყენეთ როგორც ინდექსი ამ სიაში, მაგალითად მომხარებელმა შემოიტანა 5, უნდა გამოიტანოთ მე 5 ინდექსზე მდგომი ელმენტი
+num=[1,2,3,4,5,6,7,8,9,10]
+number=int(input("enter your number: "))
+if number==0:
+    print(1)
+if number==1:
+    print(2)
+if number==2:
+    print(3)
+if number==3:
+    print(4)
+if number==4:
+    print(5)
+if number==5:
+    print(6)
+if number==6:
+    print(7)
+if number==7:
+    print(8)
+if number==8:
+    print(9)
+if number==9:
+    print(10)
+
+
+
+
