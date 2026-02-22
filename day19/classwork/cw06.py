049c6ad908ad3c7c0220343980e80d5f842906ae
gio755343-wq <gio755343@gmail.com>
2026-02-22 20:51:08 +0400

    1

---
+++ day19/classwork/cw06.py
@@ -0,0 +1,6 @@
+# 6) მომხარებელს შემოატანინე სტრინგი. შეამოწმე — სულ დიდი ასოებით არის თუ არა. თუ არის დაპრინტე "string  is uppercase", სხვა შემთხვევაში "string is lowercase"
+name="gega"
+if name.isupper:
+    print("string is uppercase")
+else:
+    print("string is lowercase")
---
+++ day19/classwork/cw07.py
@@ -0,0 +1,6 @@
+# 7) მომხარებელს შემოატანინე სტრინგი. შეამოწმე — სულ პატარა ასოებით არის თუ არა. თუ არის დაპრინტე "string is lowercase", სხვა შემთხვევაში "string is uppercase"
+name="gega"
+if name.islower:
+    print("string is lowercase")
+else:
+    print("string is uppercase")
