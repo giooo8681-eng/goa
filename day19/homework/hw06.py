1bcdc8c7e9d26fa2e9e87578a05dd487d992d546
gio755343-wq <gio755343@gmail.com>
2026-03-05 21:07:07 +0400

    1

---
+++ day19/homework/hw06.py
@@ -0,0 +1,4 @@
+# 6) მომხმარებელს შემოატანინეთ წინადადება და სიმბოლო. თქვენი დავალებაა დაითვალოთ რამდენჯერ გვხვდება ეს სიმბოლო წინადადებაში.
+name=input("enter your name: ")
+symbol=input("enter your symbol: ")
+print(name.count(symbol))
