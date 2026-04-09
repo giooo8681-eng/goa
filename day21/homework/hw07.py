a6226083d25eedbe0aca3ee8ae8edb81de153ebb
gio755343-wq <gio755343@gmail.com>
2026-04-09 19:29:31 +0400

    1

---
+++ day21/homework/hw07.py
@@ -0,0 +1,4 @@
+# 7)შექმენი ფუნქცია Find_letter,სადაც გადაცემ სტრინგს.შენი დავალება არის რომ ამ სტრინგიდან დაითვალო რამდენი a ურევია მასში და გამოიტანე ეკრანზე
+def find_letter(string):
+    return string.count("a")
+print(find_letter("gega")) 
